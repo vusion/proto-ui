e7df692258ac358a1c70a08f281a027f96184ae4
Rainfore <rainforest92@126.com>
2017-03-12 15:58:31 +0800

    :heavy_plus_sign: Add its own dependencies.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-import Sample from './src/Sample.vue';
+import Base from 'v-base.vue';
+import Button from 'v-button.vue';
+import CircularProgress from 'v-circular-progress.vue';
+import LinearProgress from 'v-linear-progress.vue';
+import ListView from 'v-list-view.vue';
+import Pager from 'v-pager.vue';
+import Sample from 'v-sample.vue';
 
-export { Sample };
+export {
+    Base,
+    Button,
+    CircularProgress,
+    LinearProgress,
+    ListView,
+    Pager,
+    Sample,
+};
