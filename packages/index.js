f4f6597b9b89c6fde204d2b27dfbeeac915c06c7
Rainfore <rainforest92@126.com>
2017-10-30 10:22:44 +0800

    :wrench: Add libraryPath in config

---
+++ packages/index.js
@@ -0,0 +1,56 @@
+import './u-base.vue/base.css';
+import * as directives from './u-base.vue/directives';
+
+import Button from './u-button.vue';
+import Checkbox from './u-checkbox.vue';
+import CircularProgress from './u-circular-progress.vue';
+import Emitter from './u-emitter.vue';
+import Field from './u-field.vue';
+import Form from './u-form.vue';
+import FormItem from './u-form-item.vue';
+import Icon from './u-icon.vue';
+import Input from './u-input.vue';
+import Label from './u-label.vue';
+import LinearProgress from './u-linear-progress.vue';
+import LinearLayout from './u-linear-layout.vue';
+import Link from './u-link.vue';
+import ListView from './u-list-view.vue';
+import ListViewItem from './u-list-view-item.vue';
+import TreeView from './u-tree-view.vue';
+import TreeViewNode from './u-tree-view-node.vue';
+import TreeViewText from './u-tree-view-text.vue';
+import Pagination from './u-pagination.vue';
+import RouterItem from './u-router-item.vue';
+import Sample from './u-sample.vue';
+import Switch from './u-switch.vue';
+import Tabs from './u-tabs.vue';
+import Tab from './u-tab.vue';
+import Textarea from './u-textarea.vue';
+
+export {
+    Button,
+    Checkbox,
+    CircularProgress,
+    Emitter,
+    Field,
+    Form,
+    FormItem,
+    Icon,
+    Input,
+    Label,
+    LinearProgress,
+    LinearLayout,
+    Link,
+    ListView,
+    ListViewItem,
+    TreeView,
+    TreeViewNode,
+    TreeViewText,
+    Pagination,
+    RouterItem,
+    Sample,
+    Switch,
+    Tabs,
+    Tab,
+    Textarea,
+};
