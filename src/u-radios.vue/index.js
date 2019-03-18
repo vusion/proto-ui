3abbfa9dfdda6200cf071608bbf1f6250bfbde54
haoxutong <1255965516@qq.com>
2019-03-18 20:41:06 +0800

    :bug: Fix change $event value error

--- src/u-radios.vue/index.js
+++ src/u-radios.vue/index.js
@@ -21,8 +21,8 @@ export default {
         },
         selectedVM(selectedVM, oldVM) {
             this.$emit('change', {
-                value: selectedVM ? selectedVM.value : undefined,
-                oldValue: oldVM ? oldVM.value : undefined,
+                value: selectedVM ? selectedVM.label : undefined,
+                oldValue: oldVM ? oldVM.label : undefined,
                 item: selectedVM ? selectedVM.item : undefined,
                 itemVM: selectedVM,
             });
