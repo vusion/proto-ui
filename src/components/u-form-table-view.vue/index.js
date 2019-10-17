9e3b2f29195a365cd849696f74636b9db89fb0bb
Rainfore <rainforest92@126.com>
2019-10-17 16:07:18 +0800

    :bug: Fix bug of <u-form-table-view>

--- src/components/u-form-table-view.vue/index.js
+++ src/components/u-form-table-view.vue/index.js
@@ -1,10 +1,11 @@
 import i18n from './i18n';
 import MDynamic from '../m-dynamic.vue';
+import UValidator from '../u-validator.vue';
 
 export const UFormTableView = {
     name: 'u-form-table-view',
     i18n,
-    mixins: [MDynamic],
+    mixins: [MDynamic, UValidator],
     props: {
         border: { type: Boolean, default: false },
         dynamic: { type: Boolean, default: false },
